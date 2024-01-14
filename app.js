035df5d5ed80714c4672b774e6684997a4244e80
jos3376 <jr3376hotmail.com>
2024-01-14 18:50:26 -0300

    subo el maquetado de EMILIA COLE

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express();
+const path = require("path");
+const port = 3030;
+
+app.use(express.static('public'));//sirve archivos estáticos//
+// con éste codigo conecto los nav //
+app.get('/home', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/index.html'));
+});
+app.get('/contact', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/contact.html'));
+});
+app.get('/music', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/music.html'));
+});
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/about.html'));
+});
+app. listen(port,()=> console.log(`http://localhost:${port}`))
+
+/*INICIO EL SERVIDOR CON npm y- luego npm i express*/
